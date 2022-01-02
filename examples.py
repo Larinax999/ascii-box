f1c16cd9e04e46036d6fdcd89f52441d0c372cd9
Larina <49904492+Larinax999@users.noreply.github.com>
2022-01-03 04:15:36 +0700

    example :)

---
+++ examples.py
@@ -0,0 +1,7 @@
+import box
+
+print(box.box1(["test1","test22","test333"]))
+
+print(box.box1(["test1","test22","test333"],20))
+
+print(box.box2({"test1":"> bruh 1","test22":"> bruh 222","test333":"> this bruh 333333"}))
